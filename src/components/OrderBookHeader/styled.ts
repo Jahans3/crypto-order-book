b9eb2ca1bbabb2424b2a47db75c9004aacff69bf
Josh Jahans <josh.jahans3@gmail.com>
2021-10-07 01:20:22 +0100

    fix(OrderBookHeader): fix clipping border by calc'ing width against horizontal border width

--- src/components/OrderBookHeader/styled.ts
+++ src/components/OrderBookHeader/styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   background: ${(props) => props.theme.main};
   border: 3px solid ${(props) => props.theme.secondary};
-  width: 100%;
+  width: calc(100% - 6px);
   display: flex;
   align-items: center;
   justify-content: flex-start;
